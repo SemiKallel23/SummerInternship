8dad439d35e2e8c62099abfecf71b384464bff2a
semikallel <semikallel23@gmail.com>
2024-07-05 12:19:30 +0100

    Second push

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
-output "mavariable" {
-    value="Hello Semi KALLEL !!!"
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "MyResourceGroup"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aks-cluster-dns"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_DS2_v2"
+    os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
